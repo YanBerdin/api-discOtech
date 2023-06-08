a2b33858ce97a912ec8679b1cfb814f95b259b17
Romain-GRADELET <romain.gradelet73@outlook.fr>
2023-06-08 16:59:41 +0200

    make controller with road: add, edit and delete

--- Back/src/Controller/Api/AlbumController.php
+++ Back/src/Controller/Api/AlbumController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Album;
 use App\Repository\AlbumRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class AlbumController extends AbstractController
 {
@@ -28,7 +31,7 @@ class AlbumController extends AbstractController
             200,
             // HTTP headers
             [],
-            // Seraialization contexts
+            // Serialization contexts
             [
                 "groups" =>
                 [
@@ -40,7 +43,7 @@ class AlbumController extends AbstractController
     }
 
     /**
-     * Sélection d'un Album
+     * Select specific album
      *
      * @param int $id
      * @param AlbumRepository $albumRepository
@@ -53,9 +56,9 @@ class AlbumController extends AbstractController
         // find all album
         $album = $albumRepository->find($id);
 
-        // manage 404
+        // 404 management
         if ($album === null){
-            return $this->json(["message"=>"Cette album n'existe pas"], Response::HTTP_NOT_FOUND);
+            return $this->json(["message"=>"Cet album n'existe pas"], Response::HTTP_NOT_FOUND);
         }
 
         return $this->json($album,200,[],[
@@ -67,6 +70,122 @@ class AlbumController extends AbstractController
             ]
         );
     }
+
+    /**
+     * Add new Album
+     *
+     * @param Request $request
+     * @param SerializerInterface $serializerInterface
+     * @param AlbumRepository $albumRepository
+     * @return JsonResponse
+     * 
+     * @Route("/api/albums", name="app_api_album_add", methods={"POST"})
+     */
+    public function add (Request $request, SerializerInterface $serializerInterface, AlbumRepository $albumRepository )
+    {
+        // Select Json content
+        $jsonContent = $request->getContent();
+
+        //
+        $newAlbum = $serializerInterface->deserialize(
+            // data to transform
+            $jsonContent,
+            // Type of object we want to deserialize
+            Album::class,
+            // Format
+            "json"
+            
+        );
+
+        $albumRepository->add($newAlbum, true);
+
+        return $this->json(
+            // data
+            $newAlbum,
+            // status code
+            Response::HTTP_CREATED,
+            // headers
+            [],
+            // context
+            [
+                "groups" =>
+                [
+                    "album_read"
+                ]
+            ]
+        );     
+    }
+
+    /**
+     * Edit specific album
+     *
+     * @param int $id
+     * @param Request $request
+     * @param AlbumRepository $albumRepository
+     * @param SerializerInterface $serializerInterface
+     * @return JsonResponse
+     * 
+     * @Route("api/albums/{id}",name="app_api_album_edit", requirements={"id"="\d+"}, methods={"PUT", "PATCH"})
+     */
+    public function edit($id, Request $request, AlbumRepository $albumRepository, SerializerInterface $serializerInterface)
+    {
+        // 1. Select Json content
+        $jsonContent = $request->getContent();
+
+        // 2. Find album on Database
+        $album = $albumRepository->find($id);
+
+        // 3. deserialize and update
+        $serializerInterface->deserialize( 
+            // data
+            $jsonContent,
+            // type
+            Album::class, 
+            // Format
+            "json",
+            // context
+            [AbstractNormalizer::OBJECT_TO_POPULATE => $album]
+        );
+
+        $albumRepository->add($album,true);
+
+        return $this->json(
+            // data
+            $album,
+            // code status
+            Response::HTTP_OK,
+            // headers
+            [],
+            // context
+            [
+                "groups" =>
+                [
+                    "album_read",
+                    "genre_browse"
+                ]
+            ]
+
+        );
+
+    }
+
+    /**
+     * Delete specific album
+     *
+     * @param int $id
+     * @param AlbumRepository $albumRepository
+     * 
+     * @Route("api/albums/{id}",name="app_api_album_delete", requirements={"id"="\d+"}, methods={"DELETE"})
+     */
+    public function delete($id, AlbumRepository $albumRepository)
+    {
+        $album = $albumRepository->find($id);
+        $albumRepository->remove($album,true);
+
+        return $this->json(null, Response::HTTP_NO_CONTENT);
+
+    }
+
 
 
 
