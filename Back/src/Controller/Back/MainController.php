6ade85d10bba83754a3923b0223a36a9d104f83e
GaetanLeborgne <gaetanleborgne.59400@hotmail.com>
2023-06-09 18:03:43 +0200

    Make backoffice MainController; twig;  and navbar

---
+++ Back/src/Controller/Back/MainController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Back;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     *@Route("/back/main", name="app_back_default")
+     */
+    public function index(): Response
+    {
+        return $this->render('back/main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
