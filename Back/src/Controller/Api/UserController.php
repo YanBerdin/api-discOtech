7f1625d2eef7ab2b800e52cf6489dede86a39d66
Romain-GRADELET <romain.gradelet73@outlook.fr>
2023-06-16 17:06:17 +0200

    Wip Api\UserController

--- Back/src/Controller/Api/UserController.php
+++ Back/src/Controller/Api/UserController.php
@@ -17,16 +17,16 @@ class UserController extends AbstractController
     /**
      * @Route("/api/user/signUp", name="app_api_user_signUp", methods={"POST"})
      */
-    public function SignUp(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher): JsonResponse
+    public function SignUp(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher)
     {
 
         // TODO : Voir comment on va recevoir les information du Front pour setter les infos en BDD
 
-        $email = $request->get("email");
-        $password = $request->get("password");
-        $firstname = $request->get("firstname");
-        $lastname = $request->get("lastname");
-        $avatar =$request->get("avatar");
+        // $email = $request->get("email");
+        // $password = $request->get("password");
+        // $firstname = $request->get("firstname");
+        // $lastname = $request->get("lastname");
+        // $avatar =$request->get("avatar");
         
         //$existingUser = $userRepository->findByEmail($email);
 
@@ -38,29 +38,29 @@ class UserController extends AbstractController
         //         Response::HTTP_OK);
         // }
         // else{
-            $user = new User();
+        //     $user = new User();
             
-            $user->setEmail($email);
+        //     $user->setEmail($email);
 
-            $user->setRoles(["ROLE_USER"]); //* Normalement OK 
+        //     $user->setRoles(["ROLE_USER"]); //* Normalement OK 
 
-            $plaintextPassword =  $password; //TODO : Récupération du password depuis le front
-            $passwordHashed = $passwordHasher->hashPassword($user, $plaintextPassword);
-            $user->setPassword($passwordHashed);
+        //     $plaintextPassword =  $password; //TODO : Récupération du password depuis le front
+        //     $passwordHashed = $passwordHasher->hashPassword($user, $plaintextPassword);
+        //     $user->setPassword($passwordHashed);
 
-            $user->setFirstname($firstname);
-            $user->setLastname($lastname);
-            $user->setAvatar($avatar);
+        //     $user->setFirstname($firstname);
+        //     $user->setLastname($lastname);
+        //     $user->setAvatar($avatar);
 
-            $userRepository->add($user,true);
+        //     $userRepository->add($user,true);
             
-        // }
-        return $this->json(
-            // data
-            ["message" => "Votre compte à bien été créé"],
-            // status code
-            Response::HTTP_CREATED,
-        );
+        // // }
+        // return $this->json(
+        //     // data
+        //     ["message" => "Votre compte à bien été créé"],
+        //     // status code
+        //     Response::HTTP_CREATED,
+        // );
     }
 
     /**
@@ -76,48 +76,48 @@ class UserController extends AbstractController
      */
     public function edit (UserRepository $userRepository, Request $request, UserPasswordHasherInterface $passwordHasher)
     {
-        /** @var User $user */
-        $user = $this->getUser();
+        // /** @var User $user */
+        // $user = $this->getUser();
      
-        $email = $request->query->get("email");
-        $password = $request->query->get("password");
-        $firstname = $request->query->get("firstname");
-        $lastname = $request->query->get("lastname");
-        $avatar =$request->query->get("avatar");
+        // $email = $request->query->get("email");
+        // $password = $request->query->get("password");
+        // $firstname = $request->query->get("firstname");
+        // $lastname = $request->query->get("lastname");
+        // $avatar =$request->query->get("avatar");
 
-        $user->setEmail($email); //! A confirmer 
+        // $user->setEmail($email); //! A confirmer 
 
-        $plaintextPassword =  $password; //TODO : Récupération du password depuis le front
-        $passwordHashed = $passwordHasher->hashPassword($user, $plaintextPassword);
-        $user->setPassword($passwordHashed);
+        // $plaintextPassword =  $password; //TODO : Récupération du password depuis le front
+        // $passwordHashed = $passwordHasher->hashPassword($user, $plaintextPassword);
+        // $user->setPassword($passwordHashed);
 
-        $user->setFirstname($firstname);
-        $user->setLastname($lastname);
-        $user->setAvatar($avatar);
+        // $user->setFirstname($firstname);
+        // $user->setLastname($lastname);
+        // $user->setAvatar($avatar);
 
-        $userRepository->add($user,true);
+        // $userRepository->add($user,true);
 
-        return $this->json(
-            // data
-            ["message" => "Votre compte à bien été modifié"],
-            // status code
-            Response::HTTP_OK,
-        );
+        // return $this->json(
+        //     // data
+        //     ["message" => "Votre compte à bien été modifié"],
+        //     // status code
+        //     Response::HTTP_OK,
+        // );
 
     }
 
     public function delete (User $user, Request $request, UserRepository $userRepository)
     {
-        if ($this->isCsrfTokenValid("edit".$user->getId(), $request->request->get("_token")))
-        {
-            $userRepository->remove($user,true);
-        }
-        return $this->json(
-            // data
-            ["message" => "Votre compte à bien été modifié"],
-            // status code
-            Response::HTTP_NO_CONTENT,
-        );
+        // if ($this->isCsrfTokenValid("edit".$user->getId(), $request->request->get("_token")))
+        // {
+        //     $userRepository->remove($user,true);
+        // }
+        // return $this->json(
+        //     // data
+        //     ["message" => "Votre compte à bien été modifié"],
+        //     // status code
+        //     Response::HTTP_NO_CONTENT,
+        // );
     }
 
 
