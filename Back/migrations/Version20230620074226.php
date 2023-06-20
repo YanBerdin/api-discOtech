4982c3a8cc8182a374e53f4de9e27b0a1d39e35a
Romain-GRADELET <romain.gradelet73@outlook.fr>
2023-06-20 09:44:21 +0200

    updade migrations

--- Back/migrations/Version20230620074226.php
+++ Back/migrations/Version20230620074226.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230616150317 extends AbstractMigration
+final class Version20230620074226 extends AbstractMigration
 {
     public function getDescription(): string
     {
