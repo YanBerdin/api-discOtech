6a443020c002a4d228bbd52f38b4e97d38b6220a
Romain-GRADELET <romain.gradelet73@outlook.fr>
2023-06-20 12:06:31 +0200

    update method browse for favorites

--- Back/src/Controller/Api/FavoriteController.php
+++ Back/src/Controller/Api/FavoriteController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Api;
 
 use App\Repository\FavoritesRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,10 +18,17 @@ class FavoriteController extends AbstractController
      * 
      * @Route("/api/favorites", name="app_api_favorite_browse", methods={"GET"})
      */
-    public function browse(FavoritesRepository $favoriteRepository): JsonResponse
+    public function browse(FavoritesRepository $favoriteRepository, UserRepository $userRepository): JsonResponse
     {
-        // List all favorites
-        $allFavorites = $favoriteRepository->findAll();
+        /** @var User $user */
+        //$user = $this->getUser();
+
+        // * For test Only (use an existing id: check DB) =================
+        $user = $userRepository->find(2);
+        // * ==============================================================
+
+        // List all favorites for currrent user
+        $allFavorites = $user->getFavorites();
 
         return $this->json(
             // Data
@@ -40,104 +48,4 @@ class FavoriteController extends AbstractController
         );     
     }
 
-    /**
-     * Select specific Favorite
-     *
-     * @param int $id
-     * @param FavoritesRepository $favoriteRepository
-     * @return JsonResponse
-     * 
-     * @Route("api/favorites/{id}",name="app_api_favorite_read", requirements={"id"="\d+"}, methods={"GET"})
-     */
-
-     public function read($id,FavoritesRepository $favoriteRepository): JsonResponse
-     {
-         $favorite = $favoriteRepository->find($id);
-         // 404 Managment
-         if ($favorite === null){
-            
-             return $this->json(
-                 // error message
-                 [
-                     "message" => "Ce favoris n'existe pas"
-                 ],
-                 // status code : 404
-                 Response::HTTP_NOT_FOUND
-             );
-         }
-         return $this->json(
-             // Alone favorite data
-             $favorite, 
-             //code return
-             200, 
-             //header HTTP
-             [], 
-             //context of serialization
-             [
-                 "groups" => 
-                 [
-                     "favorite_browse",
-                    
-                 ]
-             ]);
-     }
-
-      /**
-     * Add new favorites
-     * 
-     * @param Request $request 
-     * @param SerializerInterface $serializerInterface
-     * @param FavoritesRepository $favoriteRepository
-     * @return JsonResponse
-     * 
-     * @Route("/api/favorites", name="app_api_favorite_add", methods={"POST"})
-     */
-
-    public function add(Request $request, SerializerInterface $serializerInterface, FavoritesRepository $favoriteRepository)
-    {
-        // Select Json content
-        $jsonContent =$request->getContent();
-
-        $newFavorite = $serializerInterface->deserialize(
-            // data to transform
-            $jsonContent,
-            // type to object we want to deserialized
-            Favorite::class,
-            // Format
-            "json"
-        );
-        $favoriteRepository->add($newFavorite, true);
-        return $this->json(
-            // data
-            $newFavorite,
-            // status code
-            Response::HTTP_CREATED,
-            //headers
-            [],
-            //context
-            [
-                "groups"=>
-                [
-                    "favorite_browse"
-                ]
-            ]
-        );
-    }
-
-    /**
-     * Delete specific favorite
-     * 
-     * @param int $id
-     * @param FavoritesRepository $favoriteRepository
-     * 
-     * @Route("api/favorites/{id}",name="app_api_favorite_delete", requirements={"id"="\d+"}, methods={"DELETE"})
-     */
-
-     public function delete ($id, FavoritesRepository $favoriteRepository)
-     {
-        $favorite = $favoriteRepository->find($id);
-        $favoriteRepository->remove($favorite, true);
-
-        return $this->json(null, Response::HTTP_NO_CONTENT);
-     }
 }
