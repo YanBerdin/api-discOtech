6a87a4fac55ec82c8eecc369b8a18a463650c5f7
GaetanLeborgne <gaetanleborgne.59400@hotmail.com>
2023-06-22 09:02:37 +0200

    V2.1 paginator

--- Back/src/Controller/Back/AlbumController.php
+++ Back/src/Controller/Back/AlbumController.php
@@ -21,40 +21,47 @@ class AlbumController extends AbstractController
      */
     public function index(AlbumRepository $albumRepository, PaginatorInterface $paginator, Request $request): Response
     {
-
+        // Get the sorting order from the request, default to 'ASC' if not provided
         $order = $request->query->get('order', 'ASC');
-
+        
+        // Get the limit of items per page from the request, default to 20 if not provided
+        $limit = $request->query->getInt('limit', 20);
+    
+        // Retrieve albums from the repository based on the provided sorting order
         $albums = $albumRepository->findByAlbumOrder($order);
-
-        $pagination = $paginator->paginate($albums, $request->query->getInt('page', 1),20 );
-
+    
+        // Paginate the albums using the PaginatorInterface
+        $pagination = $paginator->paginate($albums, $request->query->getInt('page', 1), $limit);
+    dd($pagination);
         return $this->render('back/album/index.html.twig', [
             'albums' => $pagination,
             'order' => $order,
+            'limits' => [10, 20, 50], // List of options for the number of items per page
+            'currentLimit' => $limit, // Currently selected value for the number of items per page
         ]);
     }
-
     /**
      * @Route("/new", name="app_back_album_new", methods={"GET", "POST"})
      */
-    public function new(Request $request, AlbumRepository $albumRepository): Response
-    {
-        $album = new Album();
-        $form = $this->createForm(AlbumType::class, $album);
-        $form->handleRequest($request);
+public function new(Request $request, AlbumRepository $albumRepository): Response
+{
+    $album = new Album();
+    $form = $this->createForm(AlbumType::class, $album);
+    $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $albumRepository->add($album, true);
+    if ($form->isSubmitted() && $form->isValid()) {
+        // Add the album to the repository and persist it
+        $albumRepository->add($album, true);
 
-            return $this->redirectToRoute('app_back_album_index', [], Response::HTTP_SEE_OTHER);
-        }
+        // Redirect to the album index page
+        return $this->redirectToRoute('app_back_album_index', [], Response::HTTP_SEE_OTHER);
+    }
 
         return $this->renderForm('back/album/new.html.twig', [
             'album' => $album,
-            'form' => $form,
-        ]);
-        
-    }
+        '   form' => $form,
+    ]);
+}
 
     /**
      * @Route("/{id}", name="app_back_album_show", methods={"GET"})
@@ -71,30 +78,38 @@ class AlbumController extends AbstractController
      */
     public function edit(Request $request, Album $album, AlbumRepository $albumRepository): Response
     {
+        // Create the form for editing the album, pre-filled with the album's data
         $form = $this->createForm(AlbumType::class, $album);
         $form->handleRequest($request);
-
+    
         if ($form->isSubmitted() && $form->isValid()) {
+            // Save the updated album to the repository and persist it
             $albumRepository->add($album, true);
-
+    
+            // Redirect to the album index page
             return $this->redirectToRoute('app_back_album_index', [], Response::HTTP_SEE_OTHER);
         }
-
+    
+        // Render the edit form template, passing the album entity and the form to the view
         return $this->renderForm('back/album/edit.html.twig', [
             'album' => $album,
             'form' => $form,
         ]);
     }
+    
 
     /**
      * @Route("/{id}", name="app_back_album_delete", methods={"POST"})
      */
     public function delete(Request $request, Album $album, AlbumRepository $albumRepository): Response
     {
+        // Check if the CSRF token is valid for the delete action
         if ($this->isCsrfTokenValid('delete'.$album->getId(), $request->request->get('_token'))) {
+            // Remove the album from the repository and persist the changes
             $albumRepository->remove($album, true);
         }
-
+    
+        // Redirect to the album index page
         return $this->redirectToRoute('app_back_album_index', [], Response::HTTP_SEE_OTHER);
     }
 }
